bcbef8b46a7a1b01c715e4d8cc082bb7c669177a
genomics-kl <kin.lau@vai.org>
2022-11-09 21:45:58 -0500

    Add useful scripts for parsing output files

---
+++ scripts/split_vcf_by_sample.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+set -u
+set -o pipefail
+
+module load bbc/bcftools/bcftools-1.12
+
+
+file="analysis/final/07_snpEff/all.merged.filt.PASS.snpEff.vcf.gz"
+basenm=$(basename $file)
+
+mkdir -p homo_alt
+mkdir -p per_sample_vcf
+
+for sample in `bcftools query -l $file`; do
+  bcftools view --min-ac 2 --genotype hom -Ov -s $sample -o ./homo_alt/${basenm/.vcf*/.$sample.vcf} $file # extract sample and keep only sites with 2 ALT allele counts and is homozygous
+  bcftools view --min-ac 1 -Ov -s $sample -o ./per_sample_vcf/${basenm/.vcf*/.$sample.vcf} $file # extract sample and keep only sites with at least one ALT allele count
+done
